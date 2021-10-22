16249a624667df3807511303e2522fe9ccb2709b
Németh Dávid <nemeth.david.9733@gmail.com>
2021-10-22 10:30:26 +0200

    Main page, Change default text

--- resources/views/layouts/footers/auth.blade.php
+++ resources/views/layouts/footers/auth.blade.php
@@ -3,7 +3,6 @@
         <nav class="float-left">
 Edited by Gergő Kiss, Viktor Kiss and Németh Dávid<br>
 for Architecture and Softwere technology class.
-        </ul>
         </nav>
         <div class="copyright float-right">
         &copy;
--- resources/views/layouts/footers/guest.blade.php
+++ resources/views/layouts/footers/guest.blade.php
@@ -2,7 +2,6 @@
     <div class="container">
         <nav class="float-left">
 Edited by Gergő Kiss, Viktor Kiss and Németh Dávid for Architecture and Softwere technology class.
-        </ul>
         </nav>
         <div class="copyright float-right">
         &copy;
